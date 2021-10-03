3d8e18c9c2a1036b6db426920a6c462b1df55d18
mitali2608 <mitalibawankule21@gmail.com>
2021-10-03 22:51:07 +0530

    Created class to append given data

---
+++ UC3AppendData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_PP1
+{
+    class UC3AppendData
+    {
+        internal UC1Node head;
+
+        internal void AddToFront(int data)
+        {
+            UC1Node UC1Node = new UC1Node(data);
+            UC1Node.next = head;
+            head = UC1Node;
+
+            Console.WriteLine("{0} inserted into the linked list", UC1Node.data);
+        }                              //UC2 - Data added to the list
+
+        internal void AddToEnd(int data)     //UC3 Appending data to list
+        {
+
+            UC1Node UC1Node = new UC1Node(data);
+
+            if (this.head == null)
+
+                this.head = UC1Node;
+
+            else
+            {
+                UC1Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = UC1Node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", UC1Node.data);
+
+        }
+
+        internal void PrintList()
+        {
+            UC1Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
