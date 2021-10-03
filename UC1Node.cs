061e5d0ac9eaf73efa25365b1c17d4bd4ee35637
mitali2608 <mitalibawankule21@gmail.com>
2021-10-03 22:46:54 +0530

    Created Simple Linked List to the class

---
+++ UC1Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_PP1
+{
+    class UC1Node
+    {
+        public int data;
+        public UC1Node next;
+        public UC1Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
